5fd067f98c2205111db814bb0813f4c543ad9a48
Ulisses Loureiro <ulisses.loureiro@europ-assistance.pt>
2021-12-18 05:43:06 +0000

    Add Handler

--- csharpcore/GildedRose/Models/AgedBrie.cs
+++ csharpcore/GildedRose/Models/AgedBrie.cs
@@ -1,14 +1,31 @@
 ﻿using GildedRose.Services.Contracts;
+using GildedRose.Services.Implementations;
 using GildedRoseKata;
-using System;
 
 namespace GildedRose.Models
 {
     public class AgedBrie : Item, IItem
     {
+        private readonly AgedBrieHandler _handler;
+
+        public AgedBrie(int sellIn, int quality)
+        {
+            _handler = new AgedBrieHandler();
+            Name = "Aged Brie";
+            Quality = quality;
+            SellIn = sellIn;
+        }
+
+        //return the object for unit testing purposes
         public Item UpdateItem(Item item)
         {
-            throw new NotImplementedException();
+            _handler.UpdateQuality(ref item);
+            _handler.CheckQualityMaxValue(ref item);
+            item.CheckQualityMinValue();
+            item.UpdateSellIn();
+            return item;
         }
+
+
     }
 }
