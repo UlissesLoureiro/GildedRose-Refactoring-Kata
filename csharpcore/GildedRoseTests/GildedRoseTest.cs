62955bd827af7251a232f4d38e02ad0466c73611
Ulisses Loureiro <ulisses.loureiro@europ-assistance.pt>
2021-12-20 18:19:38 +0000

    some unit tests

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -1,4 +1,6 @@
-﻿using GildedRoseKata;
+﻿using GildedRose.Models;
+using GildedRose.Services.Contracts;
+using GildedRoseKata;
 using System.Collections.Generic;
 using Xunit;
 
@@ -7,12 +9,69 @@ namespace GildedRoseTests
     public class GildedRoseTest
     {
         [Fact]
-        public void foo()
+        public void ItemNameShouldNotBeModified()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            string actual = "foo";
+            IList<GenericItem> Items = new List<GenericItem> { new GenericItem { Name = actual, SellIn = 0, Quality = 0 } };
+            GildedRoseKata.GildedRose app = new GildedRoseKata.GildedRose(Items);
+            app.UpdateQualityNewVersion();
+            Assert.Equal(actual, Items[0].Name);
+        }
+
+        [Fact]
+        public void GenericItemShouldUpdateSellIn()
+        {
+            GenericItem item = new GenericItem("", 5, 6);
+
+            var actual = item.SellIn;
+
+
+            item.UpdateItem();
+
+            var expected = actual - 1;
+
+            Assert.Equal(item.SellIn, expected);
+        }
+
+        [Fact]
+        public void SulfurasItemQualityShoudBe80()
+        {
+            Sulfuras item = new Sulfuras(5, 6);
+
+
+
+            item.UpdateItem();
+
+            var expected = 80;
+
+            Assert.Equal(item.Quality, expected);
+        }
+
+        [Fact]
+        public void SulfurasItemShouldNotUpdateSellIn()
+        {
+            Sulfuras item = new Sulfuras(5, 6);
+
+            var actual = item.SellIn;
+
+
+            item.UpdateItem();
+
+            var expected = actual;
+
+            Assert.Equal(item.SellIn, expected);
+        }
+
+        [Fact]
+        public void AgedBrieItemQualityShouldBeGreaterAfterUpdate()
+        {
+            AgedBrie item = new AgedBrie(5, 6);
+
+            var actual = item.Quality;
+
+            item.UpdateItem();
+
+            Assert.True(item.Quality > actual);
         }
     }
 }
